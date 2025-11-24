2e9ebf29736d875a145af0b21eb16a1149a6c8dd
mac <mac@MacBook-Pro-de-mac.local>
2025-11-24 18:23:49 +0100

    fix errors

---
+++ frdmmcxn947_Project/component/osa/fsl_os_abstraction_bm.h
@@ -0,0 +1,12 @@
+#ifndef FSL_OS_ABSTRACTION_BM_H_
+#define FSL_OS_ABSTRACTION_BM_H_
+
+/*
+ * Dummy bare-metal OS abstraction header.
+ *
+ * In diesem Projekt verwenden wir FreeRTOS (fsl_os_abstraction_free_rtos.c),
+ * daher werden die Inhalte dieses Headers NICHT benötigt.
+ * Er existiert nur, damit der Include in fsl_os_abstraction.h kompiliert.
+ */
+
+#endif /* FSL_OS_ABSTRACTION_BM_H_ */
